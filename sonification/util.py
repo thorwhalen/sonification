5b9b58112f74fead24fe743f6afa578d3b23b5a2
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2024-10-18 15:59:22 +0200

    refactor: Moved converters to tonal package

--- sonification/util.py
+++ sonification/util.py
@@ -7,6 +7,7 @@ from config2py import (
     process_path,
     simple_config_getter,
 )
+from tonal import DFLT_SOUNDFONT
 
 pkg_name = 'sonification'
 
@@ -18,6 +19,3 @@ get_config = simple_config_getter(pkg_name)
 DFLT_OUTPUT_NAME = 'audio_output'
 DFLT_MIDI_OUTPUT = f"{DFLT_OUTPUT_NAME}.mid"
 DFLT_WAV_OUTPUT = f"{DFLT_OUTPUT_NAME}.wav"
-DFLT_SOUNDFONT = process_path(
-    get_config('SONIFY_DFLT_SOUNDFONT_PATH'),
-)
